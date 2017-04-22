c731d948e130f2db27b8b38f21edaa553ea9dde0
KevinKoek <nightarrowdevelopers@gmail.com>
2017-04-22 11:46:28 +0200

    BT57

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,11 +2,22 @@
 
 #include "BattleTank.h"
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
 
-
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet)
+	{
+		return;
+	}
+		RightTrack = RightTrackToSet;
+		LeftTrack = LeftTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -5,6 +5,7 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
 /**
  * 
  */
@@ -14,6 +15,11 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 
 public:
+		UFUNCTION(BlueprintCallable, Category = Setup)
+	    void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 		UFUNCTION(BlueprintCallable, Category = Input)
 		void IntendMoveForward(float Throw);	
+private: 
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
 };
